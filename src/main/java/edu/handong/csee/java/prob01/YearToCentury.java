d4831ac2db3c4cc85bad1a5ba4214336389be684
Jihye Chae <cowl8139@gmail.com>
2018-04-05 17:26:52 +0900

    final commit

--- src/main/java/edu/handong/csee/java/prob01/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/prob01/YearToCentury.java
@@ -2,53 +2,57 @@ package edu.handong.csee.java.prob01;
 
 import java.util.Scanner;
 
-/**
- * This class defines way to change from year to century.
- * When user input year, this program tells century that the year belongs to.
- * I love a java programming class!!!
- * I want to be a good java programmer!
- * 
- * @author Jihye Chae
- * 2018.04.05
- */
-
 public class YearToCentury {
+	/**
+	 * This class defines way to change from year to century. 
+	 * When user input year, this program tells century that the year belongs to. 
+	 * I love a java programming class!!! I want to be a good java programmer!
+	 * 
+	 * @author Jihye Chae 2018.04.05
+	 */
 	int year; // define variable
-	
-	public YearToCentury() //initialize via a constructor
+
+	public YearToCentury() // initialize via a constructor
 	{
 		year = 0; // initialize year
-	}
-	
-	public YearToCentury(int year)
-	{
-		this.year = year;
-	}
-	
-	public int yr_to_century()
-	{
-		if (year % 100 == 0)
-		{
-			year = (year / 100) - 1;
-		}
-		else
-		{
-			year = (year / 100);
-		}
-		return year;
+		/**
+		 * This method defines a constructor.
+		 * It is initialize via a constructor.
+		 * 
+		 * @author Jihye Chae 2018.04.05
+		 */
 	}
 
-	public static void main(String[] args) {
+	public YearToCentury(int year) {
+		this.year = year; // convert
+		/**
+		 * This method is for converting.
+		 * 
+		 * @author Jihye Chae 2018.04.05
+		 */
+	}
+
+	public int yr_to_century() // year to century
+	{
+		if (year % 100 == 0) // e.g 1900, 2000
+		{
+			year = (year / 100) + 1; // e.g 1900 -> 19 +1
+		} else {
+			year = (year / 100); // e.g 1994, 2018 -> 20th, 21st
+		}
+		return year; // return year
+	}
+
+	public static void main(String[] args) { // main
 		// TODO Auto-generated method stub
-		int yr = 0;
-		Scanner keyboard = new Scanner(System.in);
-		
-		System.out.println("Input year: ");
-		yr = keyboard.nextInt();
-		
-		YearToCentury year = new YearToCentury(yr);
-		System.out.println(yr + " is " + year.yr_to_century() + "th century.");
-		
+		int yr = 0; // initialize yr
+		Scanner keyboard = new Scanner(System.in); // scanner
+
+		System.out.println("Input year: "); // print out message
+		yr = keyboard.nextInt(); // input
+
+		YearToCentury year = new YearToCentury(yr); // change year
+		System.out.println(yr + " is " + year.yr_to_century() + "th century."); // result message
 
 	}
 
